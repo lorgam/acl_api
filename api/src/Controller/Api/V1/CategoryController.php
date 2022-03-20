18b30caf8d36474c466c5a9a3d3c5a9eec2760c9
Luis A. Martín <lorgam@gmail.com>
2022-03-20 16:15:40 +0000

    feat:Category controller

--- api/src/Controller/Api/V1/CategoryController.php
+++ api/src/Controller/Api/V1/CategoryController.php
@@ -2,20 +2,91 @@
 
 namespace App\Controller\Api\V1;
 
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+
+// @TODO: Validate Json Structure, Headers...
 
 /**
  * @Route("/category", name="api_category_")
  */
 class CategoryController extends AbstractController
 {
+
+    private $serializer;
+    private $repo;
+
+    public function __construct(SerializerInterface $serializer, CategoryRepository $repo)
+    {
+        $this->serializer = $serializer;
+        $this->repo = $repo;
+    }
+
     /**
      * @Route("/", methods={"GET"}, name="get")
      */
-    public function getCategories(): Response
+    public function getCategories(): JsonResponse
     {
-        return new Response('@TODO');
+        return $this->categoryToJsonResponse($this->repo->findAll(), ['products']);
+    }
+
+    /**
+     * @Route("/", methods={"POST"}, name="post")
+     */
+    public function postCategoy(Request $request): JsonResponse
+    {
+        $category = $this->serializer->deserialize($request->getContent(), Category::class, 'json');
+        $this->repo->add($category);
+        return $this->categoryToJsonResponse($category, ['products']);
+    }
+
+    /**
+     * @Route("/{id}", methods={"GET"}, name="get_id")
+     */
+    public function getCategory(?Category $category): JsonResponse
+    {
+        return $this->categoryToJsonResponse($category, ['products']);
+    }
+
+    /**
+     * @Route("/{id}", methods={"PUT"}, name="put_id")
+     */
+    public function putCategory(Request $request, ?Category $category): JsonResponse
+    {
+        if ($category != null) {
+            $this->serializer->deserialize($request->getContent(), Category::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $category]);
+            $this->repo->add($category);
+        }
+
+        return $this->categoryToJsonResponse($category, ['products']);
+    }
+
+    /**
+     * @Route("/{id}", methods={"DELETE"}, name="delete_id")
+     */
+    public function deleteCategory(?Category $category): JsonResponse
+    {
+        $this->repo->remove($category);
+        return new JsonResponse(['deleted' => true]);
+    }
+
+    private function categoryToJsonResponse(null|array|Category $data, array $ignoredAttributes = []): JsonResponse
+    {
+        if ($data == null) return new JsonResponse([], Response::HTTP_NO_CONTENT);
+
+        return JsonResponse::fromJsonString(
+            $this->serializer->serialize(
+                $data,
+                'json',
+                [AbstractNormalizer::IGNORED_ATTRIBUTES => $ignoredAttributes]
+            )
+        );
     }
 }

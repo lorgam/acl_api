a16c75b97fb1f6922560bcf64391829074dee21d
Luis A. Martín <lorgam@gmail.com>
2022-03-20 12:25:28 +0000

    fix:Allow deleting categories asociated to products

--- api/src/Entity/Product.php
+++ api/src/Entity/Product.php
@@ -28,6 +28,7 @@ class Product
 
     /**
      * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="products")
+     * @ORM\JoinColumn(name="category_id", referencedColumnName="id", onDelete="SET NULL")
      */
     private $category;
 
